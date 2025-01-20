158fa038fd2d3ca9ebf00e00b2afc877847d14d5
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-01-20 13:56:14 +0100

    Refactor styles and integrate Tailwind CSS with NProgress for enhanced UI transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
-	import '../app.css';
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
+	import nProgress from 'nprogress';
+	import '../app.scss';
+
 	let { children } = $props();
+
+	nProgress.configure({ showSpinner: false });
+
+	beforeNavigate(function (): void {
+		nProgress.start();
+	});
+	afterNavigate(function () {
+		nProgress.done();
+	});
 </script>
 
 {@render children()}
