0df456f5f2c1450d0aca291eb5ce25914dcf4120
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-01-20 15:20:51 +0100

    Add initial implementation of sidebar and UI components with responsive design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
 	import { afterNavigate, beforeNavigate } from '$app/navigation';
 	import nProgress from 'nprogress';
-	import '../app.scss';
+	import '../app.scss'; // global css
 
-	let { children } = $props();
+	let { children } = $props(); // to render content on the client. must be done.
 
+	/**
+	 * nProgress init
+	 */
 	nProgress.configure({ showSpinner: false });
 
 	beforeNavigate(function (): void {
 		nProgress.start();
 	});
-	afterNavigate(function () {
+	afterNavigate(function (): void {
 		nProgress.done();
 	});
 </script>
 
+<!-- render to client -->
 {@render children()}
---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import type { Snippet } from 'svelte';
+	import SidebarRoot from './components/side-bar-root.svelte';
+
+	interface props {
+		children: Snippet;
+	}
+
+	let { children }: props = $props();
+</script>
+
+<!-- DOM begins here -->
+<Sidebar.Provider>
+	<SidebarRoot />
+	<!-- #Application -->
+	<main class="flex-1">
+		<Sidebar.Trigger />
+		<!-- children -->
+		{@render children?.()}
+	</main>
+</Sidebar.Provider>
